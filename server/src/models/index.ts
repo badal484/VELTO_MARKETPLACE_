b8d332f413b94a94914798bb032769e2a3794e00
badal484 <badal90603@gmail.com>
2026-04-21 19:26:33 +0530

    add index

---
+++ server/src/models/index.ts
@@ -0,0 +1,22 @@
+import { User } from './User';
+import { Shop } from './Shop';
+import { Product } from './Product';
+import './Order';
+import './Cart';
+import './Wishlist';
+import './Conversation';
+import './Message';
+import './Review';
+import './Notification';
+import { PayoutRequest } from './PayoutRequest';
+import { WalletTransaction } from './WalletTransaction';
+import { Banner } from './Banner';
+
+export {
+  User,
+  Shop,
+  Product,
+  PayoutRequest,
+  WalletTransaction,
+  Banner
+};
