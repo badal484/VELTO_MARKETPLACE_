752a6b66c2810593a5cb10e64e8ad5232ac95ab1
badal484 <badal90603@gmail.com>
2026-04-21 19:06:55 +0530

    Fix: add client as normal folder

---
+++ client/src/navigation/RiderNavigator.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RiderNavigator = () => {
+  return (
+    <View>
+      <Text>RiderNavigator</Text>
+    </View>
+  )
+}
+
+export default RiderNavigator
