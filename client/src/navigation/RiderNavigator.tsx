7bffab3f8afa57946b277ac02cd3016ef3fd545c
badal484 <badal90603@gmail.com>
2026-04-21 20:52:37 +0530

    add rider navigator

--- client/src/navigation/RiderNavigator.tsx
+++ client/src/navigation/RiderNavigator.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import RiderDashboardScreen from '../screens/rider/RiderDashboardScreen';
+import WalletScreen from '../screens/rider/WalletScreen';
+import ProductDetailScreen from '../screens/buyer/ProductDetailScreen';
+import ChatScreen from '../screens/chat/ChatScreen';
+import {IProduct, IUser} from '@shared/types';
 
-const RiderNavigator = () => {
+export type RiderStackParamList = {
+  RiderDashboard: undefined;
+  Wallet: undefined;
+  OrderDetail: {orderId: string};
+  ProductDetail: {id: string};
+  ChatRoom: {
+    conversationId: string;
+    otherUser: Partial<IUser>;
+    productTitle?: string;
+    productId?: string;
+    orderId?: string;
+  };
+};
+
+const Stack = createStackNavigator<RiderStackParamList>();
+
+export const RiderNavigator = () => {
   return (
-    <View>
-      <Text>RiderNavigator</Text>
-    </View>
-  )
-}
-
-export default RiderNavigator
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="RiderDashboard" component={RiderDashboardScreen} />
+      <Stack.Screen name="Wallet" component={WalletScreen} />
+      <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
+      <Stack.Screen name="ChatRoom" component={ChatScreen} />
+    </Stack.Navigator>
+  );
+};
